d922b750fb9b839aba8a2de57059dd55d709aef0
karammm <karamjitverma9@gmail.com>
2020-05-22 14:02:17 +0530

    region specification

--- src/com/karam/connectfour/Controller.java
+++ src/com/karam/connectfour/Controller.java
@@ -1,4 +1,23 @@
 package com.karam.connectfour;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	@FXML
+	public GridPane rootGridPane;
+
+	@FXML
+	public Pane insertedDiscPane;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
 }
